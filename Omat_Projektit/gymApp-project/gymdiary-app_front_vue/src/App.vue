9e61db45382d811fb5bf90a71c7df05723358b01
Kristian Pekkanen <kristian.pekkanen@jamk.fi>
2024-04-11 15:26:07 +0300

    creating frontend vue project

---
+++ Omat_Projektit/gymApp-project/gymdiary-app_front_vue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+  </header>
+  <RouterView />
+</template>
+
+
---
+++ Omat_Projektit/gymApp-project/gymdiary-app_front_vue/src/components/NavBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/login">Login</RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
